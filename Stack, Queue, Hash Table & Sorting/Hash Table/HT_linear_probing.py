4bbf1bc89dc06fe285be71438b4ba30d319b649d
Muhammed Swalih M <zzray485@gmail.com>
2025-02-12 15:21:33 +0530

    heapify_down

---
+++ Stack, Queue, Hash Table & Sorting/Hash Table/HT_linear_probing.py
@@ -0,0 +1,72 @@
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size  # Directly store (key, value) pairs
+        self.size = size
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ":", val)
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % self.size
+        return my_hash
+
+    def insert_item(self, key, value):
+        index = self.__hash(key)
+        original_index = index
+
+        while self.data_map[index] is not None:  # Linear probing
+            if self.data_map[index][0] == key:  # Update existing key
+                self.data_map[index] = (key, value)
+                return
+            index = (index + 1) % self.size  # Move to next slot
+            if index == original_index:  # Full loop, no space
+                raise Exception("HashTable is full")
+
+        self.data_map[index] = (key, value)  # Insert new key-value pair
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        original_index = index
+
+        while self.data_map[index] is not None:
+            if self.data_map[index][0] == key:
+                return self.data_map[index][1]
+            index = (index + 1) % self.size  # Linear probing
+            if index == original_index:  # Full loop
+                break
+        return None  # Key not found
+
+    def delete_item(self, key):
+        index = self.__hash(key)
+        original_index = index
+
+        while self.data_map[index] is not None:
+            if self.data_map[index][0] == key:
+                self.data_map[index] = None  # Mark as deleted (lazy deletion)
+                return True
+            index = (index + 1) % self.size  # Linear probing
+            if index == original_index:
+                break
+        return False  # Key not found
+
+    def keys(self):
+        return [kv[0] for kv in self.data_map if kv is not None]
+
+
+ht = HashTable(7)
+
+ht.insert_item("apple", 10)
+ht.insert_item("banana", 20)
+ht.insert_item("grape", 30)
+
+print(ht.get_item("banana"))  # Output: 20
+print(ht.get_item("cherry"))  # Output: None
+
+ht.delete_item("banana")
+print(ht.get_item("banana"))  # Output: None
+
+ht.print_table()
+
--- Tree, Graph, Heap, BST & Trie/test.py
+++ Tree, Graph, Heap, BST & Trie/test.py
@@ -1,86 +1,32 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        
-class BinarySearchTree:
+class MinHeap:
     def __init__(self):
-        self.root = None
+        self.heap = []
         
-    def __r_insert(self, current_node, value):
-        if current_node == None:
-            return Node(value)
-        if value < current_node.value:
-            current_node.left = self.__r_insert(current_node.left, value)
-        if value > current_node.value:
-            current_node.right = self.__r_insert(current_node.right, value)
-        return current_node
-    def r_insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
-        self.__r_insert(self.root, value)
+    def _left_child(self, index):
+        return 2 * index + 1
         
-    def print_bst_visual(self, node=None, level=0, prefix='Root: '):
-        if node is None:
-            node = self.root
-            if node is None:
-                print('Tree is empty.')
-                return
-        if node.right:
-            self.print_bst_visual(node.right, level+1, 'R--- ')
-        print("  " * level + prefix + str(node.value))
-        if node.left:
-            self.print_bst_visual(node.left, level+1, 'L--- ')
-            
-    def __r_contains(self, current_node, value):
-        if current_node == None:
-            return False
-        if value == current_node.value:
-            return True
-        if value < current_node.value:
-            return self.__r_contains(current_node.left, value)
-        if value > current_node.value:
-            return self.__r_contains(current_node.right, value)
-    def r_contains(self, value):
-        return self.__r_contains(self.root, value)
+    def _right_child(self, index):
+        return 2 * index + 2
         
-    def min_value(self, current_node):
-        while current_node.left:
-            current_node = current_node.left
-        return current_node.value
-    
-    def __delete_node(self, current_node, value):
-        if current_node == None:
-            return False
-        if value == current_node.value:
-            return True
-        elif value < current_node.value:
-            return self.__delete_node(current_node.left, value)
-        elif value > current_node.value:
-            return self.__delete_node(current_node.right, value)
-        else:
-            if current_node.left == None and current_node.right == None:
-                return None
-            elif current_node.left == None:
-                current_node = current_node.right
-            elif current_node.right == None:
-                current_node = current_node.left
-            else:
-                subtree_min = self.min_value(current_node.right)
-                current_node.value = subtree_min
-                current_node.right = self.__delete_node(current_node.right, subtree_min)
-        return current_node
-    def delete_node(self, value):
-        self.root = self.__delete_node(self.root, value)
-
-my_tree = BinarySearchTree()
-my_tree.r_insert(30)
-my_tree.r_insert(20)
-my_tree.r_insert(10)
-my_tree.r_insert(40)
-my_tree.r_insert(50)
-my_tree.print_bst_visual()
-print(my_tree.r_contains(50))
-my_tree.delete_node(30)
-my_tree.print_bst_visual()
+    def _parent(self, index):
+        return (index - 1) // 2
+        
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+        
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        
+        while current:
+            if current > 0 and self.heap[current] < self.heap[self._parent(current)]:
+                self._swap(current, self._parent(current))
+                current = self._parent(current)
+                
+my_heap = MinHeap()
+my_heap.insert(1)
+my_heap.insert(2)
+my_heap.insert(3)
+my_heap.insert(4)
+my_heap.insert(5)
+print(my_heap.heap)
