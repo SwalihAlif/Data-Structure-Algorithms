ff6ba40d1cf0c38dc77c0ceeaad61c4e23d4c869
Muhammed Swalih M <zzray485@gmail.com>
2025-05-02 21:21:08 +0530

    __delitem__ added in hashtable

--- Stack, Queue, Hash Table & Sorting/Hash Table/construction.py
+++ Stack, Queue, Hash Table & Sorting/Hash Table/construction.py
@@ -26,6 +26,16 @@ class HashTable:
                     return self.data_map[index][i][1]
                     
         return None
+    
+    def __delitem__(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    del self.data_map[index][i]
+                    print(f"The key {key} is deleted")
+                    return
+        print(f"The key {key} is not found..")
             
 
 my_hash_table = HashTable()
@@ -37,3 +47,4 @@ my_hash_table.insert_item('pine apple', 145)
 my_hash_table.print_table()
 print('-------------------------------')
 print('pine apple: ', my_hash_table.get_item('pine apple'))
+my_hash_table.__delitem__('march 6')
