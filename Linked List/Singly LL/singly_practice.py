665a676118073e85eba28bb753313a24ba29d6a6
Muhammed Swalih M <zzray485@gmail.com>
2025-05-23 23:43:41 +0530

    update

--- Linked List/Singly LL/singly_practice.py
+++ Linked List/Singly LL/singly_practice.py
@@ -30,7 +30,7 @@ class LinkedList:
         return True
 
     def pop(self):
-        if self.length == 0:
+        if self.length == 0: 
             return None
         temp = self.head
         pre = self.head
